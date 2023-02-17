fa5cd6cff4e7817371a18b0a97077e7a86423f0b
Nick Nyanjui <nicknyanjui@gmail.com>
2023-02-17 23:00:16 +0000

    Add exercise.py

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -7,16 +7,43 @@ import redis
 from typing import Any, Callable
 import uuid
 
+cache = redis.Redis()
 
-def count_calls(wrapped: Callable) -> Callable:
+
+def count_calls(method: Callable) -> Callable:
     """Decorator function to count number of calls to method"""
 
-    @wraps(wrapped)
+    @wraps(method)
     def wrapper(self, args, **kwargs) -> Callable:
         """Increments the number of calls made to a function on each call"""
-        self._redis.incrby(wrapped.__qualname__, 1)
-        return wrapped
+        self._redis.incrby(method.__qualname__, 1)
+        return method
     return wrapper
+
+
+def call_history(method: Callable) -> Callable:
+    """Decorator function to store input and ouput of a method"""
+
+    @wraps(method)
+    def wrapper(self, *args, **kwargs) -> Callable:
+        """Pushes input and output to the end of a list"""
+        self._redis.rpush(f"{method.__qualname__}:inputs", str(*args))
+        result = self.__call__(method, *args, **kwargs)
+        self._redis.rpush(f"{method.__qualname__}:outputs", str(result))
+        return method
+    return wrapper
+
+
+def replay(method: Callable, cache: Any = cache) -> None:
+    """Displays the history of calls of a function"""
+    counter = cache.get(method.__qualname__).decode("utf-8")
+    print(f"{method.__qualname__} was called {counter} times:")
+    inputs = list(cache.lrange(f"{method.__qualname__}:inputs", 0, -1))
+    outputs = list(cache.lrange(f"{method.__qualname__}:outputs", 0, -1))
+    for i, o in zip(inputs, outputs):
+        print("{0}(*({1},)) -> {2}"
+              .format(method.__qualname__, repr(i.decode('utf-8')),
+                      o.decode('utf-8')))
 
 
 class Cache:
@@ -24,15 +51,20 @@ class Cache:
 
     def __init__(self):
         """Constructor method for new instances"""
-        self._redis = redis.Redis()
+        self._redis = cache
         self._redis.flushdb()
 
+    def __call__(self, method, *args, **kwargs):
+        """Caller method to execute instance method"""
+        return method(self, *args, **kwargs)
+
+    @call_history
     @count_calls
     def store(self, data: Any) -> str:
         """Generates a random key and stores the input data using the key"""
         key: str = str(uuid.uuid4())
-        # if not isinstance(data, (int, str, bytes)):
-        #    data = json.dumps(data)
+        if not isinstance(data, (int, str, bytes)):
+            data = json.dumps(data)
         self._redis.set(key, data)
         return key
 
