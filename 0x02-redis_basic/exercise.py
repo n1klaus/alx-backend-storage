31840c8d530c1633f1778733d3c13cf5f1a826a6
Nick Nyanjui <nicknyanjui@gmail.com>
2023-02-17 10:16:07 +0000

    Add exercise.py

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -3,7 +3,7 @@
 
 import json
 import redis
-from typing import Any
+from typing import Any, Callable
 import uuid
 
 
@@ -18,7 +18,29 @@ class Cache:
     def store(self, data: Any) -> str:
         """Generates a random key and stores the input data using the key"""
         key: str = str(uuid.uuid4())
-        if not isinstance(data, (str, bytes)):
+        if not isinstance(data, (int, str, bytes)):
             data = json.dumps(data)
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Callable) -> Any:
+        """Returns obj stored using provided key"""
+        value: Any = self._redis.get(key)
+        if value:
+            if fn == str:
+                return self.get_str(value, fn)
+            elif fn == int:
+                return self.get_int(value, fn)
+            elif fn is not None:
+                return fn(value)
+            else:
+                return value
+        return None
+
+    def get_str(self, value: Any, fn: Callable) -> str:
+        """Returns str from raw value"""
+        return fn(value)
+
+    def get_int(self, value: Any, fn: Callable) -> int:
+        """Returns int from raw value"""
+        return fn(value)
