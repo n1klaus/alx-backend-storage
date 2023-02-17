0e4bcb52a93474ebb237f738874c7bbf9c1161b6
Nick Nyanjui <nicknyanjui@gmail.com>
2023-02-17 09:29:18 +0000

    Add exercise.py

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Cache class model for Redis"""
+
+import json
+import redis
+from typing import Any
+import uuid
+
+
+class Cache:
+    """Cache model for Redis"""
+
+    def __init__(self):
+        """Constructor method for new instances"""
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: Any) -> str:
+        """Generates a random key and stores the input data using the key"""
+        key: str = str(uuid.uuid4())
+        if not isinstance(data, (str, bytes)):
+            data = json.dumps(data)
+        self._redis.set(key, data)
+        return key
