05053a6a7449aa1f2e7617ac39631430cce7c8da
Nick Nyanjui <nicknyanjui@gmail.com>
2023-04-17 15:11:10 +0000

    Add 102-log_stats

--- 0x01-NoSQL/101-students.py
+++ 0x01-NoSQL/101-students.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """Top students by average score"""
 
 from pymongo.collection import Collection
---
+++ 0x01-NoSQL/102-log_stats.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+"""Provides stats about Nginx logs stored in MongoDB"""
+
+if __name__ == "__main__":
+    from pymongo import MongoClient
+    from pymongo.collection import Collection
+
+    with MongoClient() as client:
+        db = client.logs
+        collection = Collection(db, "nginx")
+        print("{} logs".format(collection.count_documents({})))
+        print("Methods:")
+        methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+        for m in methods:
+            c = collection.count_documents({"method": m})
+            print("\tmethod {}: {}".format(m, c))
+        s = collection.count_documents({"method": "GET", "path": "/status"})
+        print("{} status check".format(s))
+        print("IPS:")
+        ip_occurence = collection.aggregate([
+            {
+                "$group": {
+                    "_id": "$ip",
+                    "occurence": {
+                        "$sum": 1
+                    }
+                }
+            },
+            {
+                "$sort": {
+                    "occurence": -1
+                }
+            },
+            {
+                "$limit": 10
+            }
+        ])
+        for ip in ip_occurence:
+            print(f"\t{ip.get('_id')}: {ip.get('occurence')}")
